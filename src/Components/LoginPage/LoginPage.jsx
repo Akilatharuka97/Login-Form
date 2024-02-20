895c1d9dd1bf1be96a893fe3295aaa979365e605
Akila Tharuka <akilatharuka555@gmail.com>
2024-02-20 10:32:06 +0530

    created login form

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './LoginPage.css';
+import React from "react";
+import "./LoginPage.css";
 
 const LoginPage = () => {
   return (
-    <div>
-      Login Page
+    <div className="wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="username" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="password" required />
+        </div>
+
+        <div className="remember-fogot">
+          <label>
+            <input type="checkbox" />
+            Remember Me
+          </label>
+          <a href="#">Forgot Password?</a>
+        </div>
+
+        <button type="submit">Login</button>
+
+        <div className="register-link">
+          <p>Dont't have an account? <a href="#">Register</a></p>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default LoginPage;
